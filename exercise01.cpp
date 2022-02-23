ffb52353be24133513e0331688be9605467c1a4b
it21303302 <99312076+it21303302b@users.noreply.github.com>
2022-02-23 05:55:30 +0000

    exersise 1

--- exercise01.cpp
+++ exercise01.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
+ cout << "No\t" << "Name\t\t" << "Marks" << endl;
  for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  names[r]
+     cout <<  r+1 << "\t"
+          <<  names[r]<<"\t\t"
           << marks[r] << endl;
  }
 }
