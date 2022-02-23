04fd0d6c35ece9db373024eb32f3447604262966
it21303302 <99312076+it21303302b@users.noreply.github.com>
2022-02-23 06:24:38 +0000

    exercise 3

--- exercise03.cpp
+++ exercise03.cpp
@@ -2,11 +2,16 @@
 
 using namespace std;
 
+
 int volume(int height, int width, int length);
 
 // 1. Define a structure called Box
 // have the integer data types Height, Width, Length
-
+struct box{
+  int Height; 
+  int Width;
+  int Length;
+}box1,box2;
 // Do not change the main function
 int main() {
     // 2. Create a variable called box1 of the Box structure type
@@ -14,7 +19,6 @@ int main() {
     // 3. Create a variable called box2 of the Box structure type
     
     // int box2Height, box2Width, box2Length;
-    int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
     cout << "Enter Box 1 Height : ";
@@ -32,12 +36,17 @@ int main() {
     cin >> Length of Box 2;
     
     // 5. Replace the coding below to pass the Box type structure
-    totalVolume = volume( , , )
-             + volume( , , );
+    totalVolume = volume(box1Height ,box1Width ,box1Length )
+             + volume(int box2Height ,box2Width ,box2Length );
     
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
+}
+
+int volume(int height, int width, int length)
+{
+  return height*width*length;
 }
 
 // Implement the functions here
